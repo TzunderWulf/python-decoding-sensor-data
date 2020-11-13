64bdd15334b0eb493102830e1dc7c3ca0ff71f8e
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-13 03:07:49 +0100

    Add files via upload

---
+++ sensor_app.py
@@ -0,0 +1,22 @@
+# Runner script for all modules
+from load_data import load_sensor_data 
+
+##############################
+# Do not remove these two lines
+# They are needed to validate your unittest
+data = []
+print("Sensor Data App")
+
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
+##############################
+
+# Module 1 code here:
+
+# Module 2 code here:
+
+# Module 3 code here:
+
+# Module 4 code here:
+
+# Module 5 code here:
