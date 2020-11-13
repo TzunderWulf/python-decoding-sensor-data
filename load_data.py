fbeb45b617ba7cae46f09b82e99dfd6b03043f0a
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-13 03:11:43 +0100

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -4,10 +4,13 @@ import csv
 
 def load_sensor_data(sensor_data):
     sensor_data = []
+
     sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
+
     for sensor_file in sensor_files:
         with open(sensor_file=data_file):
             data_reader = csv.DictReader(data_file, delimeter=",")
             for row in data_file:
                 row.append(sensor_data)
+                
     return sensor_data
